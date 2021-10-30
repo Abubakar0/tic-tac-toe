42c52e6528ea42c6127eca503abcd0041ea290ef
Abubakar0 <48792838+Abubakar0@users.noreply.github.com>
2021-10-30 23:36:16 +0500

    devops assignment

--- App.js
+++ App.js
@@ -8,99 +8,10 @@ import {
   View,
   Button,
   TouchableOpacity,
-  Alert,
-  Modal,
-  Pressable,
   SafeAreaView,
 } from "react-native";
-import { render } from "react-dom";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gamestate: [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-      ],
-      currentplayer: 1,
-    };
-  }
-  componentDidMount() {
-    this.initialize();
-  }
-  initialize = () => {
-    this.setState({
-      gamestate: [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-      ],
-      currentplayer: 1,
-    });
-  };
-  winner = () => {
-    const num = 3;
-    var arr = this.state.gamestate;
-    var sum;
-    for (var i = 0; i < num; i++) {
-      sum = arr[i][0] + arr[i][1] + arr[i][2];
-      if (sum == 3) {
-        return 1;
-      } else if (sum == -3) {
-        return -1;
-      }
-    }
-    //columns
-    for (var i = 0; i < num; i++) {
-      sum = arr[0][i] + arr[1][i] + arr[2][i];
-      if (sum == 3) {
-        return 1;
-      } else if (sum == -3) {
-        return -1;
-      }
-    }
-    //diagonals
-    sum = arr[0][0] + arr[1][1] + arr[2][2];
-    if (sum == 3) {
-      return 1;
-    } else if (sum == -3) {
-      return -1;
-    }
-
-    sum = arr[2][0] + arr[1][1] + arr[0][2];
-    if (sum == 3) {
-      return 1;
-    } else if (sum == -3) {
-      return -1;
-    }
-    return 0;
-  };
-  ontilepress = (row, col) => {
-    var value = this.state.gamestate[row][col];
-    if (value !== 0) {
-      return;
-    }
-
-    var player = this.state.currentplayer;
-    var arr = this.state.gamestate.slice();
-    arr[row][col] = player;
-    this.setState({ gamestate: arr });
-
-    //switch
-    var nextp = player == 1 ? -1 : 1;
-    this.setState({ currentplayer: nextp });
-    var winner = this.winner();
-    if (winner == 1) {
-      alert("Player 1 wins");
-      this.initialize();
-    } else if (winner == -1) {
-      alert("Player 2 wins");
-      this.initialize();
-    }
-  };
-
   renderIcon = (row, col) => {
     var value = this.state.gamestate[row][col];
     switch (value) {
